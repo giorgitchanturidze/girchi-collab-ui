9e4bc30583f83048dfc6513a2659584d88ccf517
giorgitchanturidze <chanturidzegiorgi31@gmail.com>
2022-08-29 14:47:00 +0400

    Adding custom collors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkButton' : '#323329',
+        'greenText' : '#327F37',
+        'lightRed' : '#F95A64', //vector 185
+        'lightBlue' : '#2F69FF', //vector 186
+        'greenButton' : '#1A8917', //vector 187 and buttons
+      }
+    },
   },
   plugins: [],
 }
